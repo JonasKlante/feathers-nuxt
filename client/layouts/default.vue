dd7dc8fad79eb31010efce455ab5aa18d7e5c052
Silvestre Herrera <silvestre.herrera@gmail.com>
2017-04-30 12:51:38 -0300

    Restore custom default layout

---
+++ client/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <nuxt />
+</template>
+
+<script>
+import '~feathers';
+
+export default {
+  name: 'default-layout',
+};
+</script>
--- client/store/index.js
+++ client/store/index.js
@@ -1,19 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import feathersVuex from 'feathers-vuex';
-import feathers from '~feathers';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {},
 });
-
-feathers.configure(feathersVuex(store, {
-  idField: '_id',
-  auth: {
-    userService: '/users',
-  },
-}));
 
 export default store;
