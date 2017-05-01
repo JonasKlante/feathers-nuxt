435123daa6d15c3587afdcd29d72083024c33822
Silvestre Herrera <silvestre.herrera@gmail.com>
2017-05-01 12:51:34 -0300

    ESLint: Resolve Nuxt webpack aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const resolve = require('path').resolve;
+
 module.exports = {
   env: {
     es6: true,
@@ -8,5 +10,25 @@ module.exports = {
   plugins: [
     'html',
     'import'
-  ]
+  ],
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: {
+          resolve: {
+            alias: {
+              '~': resolve(__dirname, 'client'),
+              '~assets': resolve(__dirname, 'client/assets'),
+              '~components': resolve(__dirname, 'client/components'),
+              '~mixins': resolve(__dirname, 'client/mixins'),
+              '~pages': resolve(__dirname, 'client/pages'),
+              '~plugins': resolve(__dirname, 'client/plugins'),
+              '~static': resolve(__dirname, 'client/static'),
+              '~store': resolve(__dirname, 'client/store'),
+            },
+          },
+        },
+      },
+    },
+  },
 };
