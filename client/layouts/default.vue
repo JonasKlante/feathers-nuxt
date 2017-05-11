abc867be1533250011c2a9d81d443e89b0cc37e3
Silvestre Herrera <silvestre.herrera@gmail.com>
2017-05-11 12:24:30 -0300

    Clean up initial client-side auth

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -7,14 +7,25 @@
 
 <script>
 import mainMenu from '~components/main-menu.vue';
+import { mapActions, mapState } from 'vuex'; // eslint-disable-line import/no-extraneous-dependencies
 
 export default {
   name: 'default-layout',
 
   components: { mainMenu },
 
+  computed: {
+    ...mapState('auth', ['user']),
+  },
+
+  methods: {
+    ...mapActions({
+      authenticate: 'auth/authenticate',
+    }),
+  },
+
   mounted() {
-    this.$store.dispatch('auth/authenticate');
+    if (this.user) this.authenticate();
   },
 };
 </script>
